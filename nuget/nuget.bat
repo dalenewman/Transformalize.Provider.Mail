c1a19e101d0cd447caac9bf02bb96618f41a0ee0
Dale Newman <dalenewman@gmail.com>
2019-11-27 16:41:37 -0500

    updated dependencies

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Provider.Mail.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Mail.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Mail.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Mail.Autofac.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Mail.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Mail.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
